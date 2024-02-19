73f0d679d7b34d2480c26da6f2d9841741665a65
NafAlam <alam.nafizul63@gmail.com>
2024-02-19 23:04:53 +0000

    implicit assignment

---
+++ 3_implicit_assignment.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// x := 1
+	// fmt.Printf("%T", x)
+	x := uint(1)
+	fmt.Printf("%T", x) // this funtion and the '%T' argument displays the type of the var
+}
